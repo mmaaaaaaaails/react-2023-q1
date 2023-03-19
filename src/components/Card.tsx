cffe09c36face6a44a7867527a74908a6c00560f
vadimdudko <v.dudko93@gmail.com>
2023-03-19 13:14:30 +0300

    feat: add card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './card.module.scss';
+import favorite from '../assets/images/favorite.svg';
+import chat from '../assets/images/chat.svg';
+
+interface ICard {
+    image: string;
+    name: string;
+    description: string;
+    likes: number;
+    comments: number;
+}
+
+class Card extends React.Component<ICard> {
+
+    render() {
+        return (
+            <div className={styles.container}>
+                <img className={styles.img} src={this.props.image} alt="photo" />
+                <p className={styles.title}>{this.props.name}</p>
+                <p className={styles.desc}>{this.props.description}</p>
+                <div className={styles.bottom}>
+                    <div className={styles.bottom__item}>
+                        <img className={styles.bottom__img} src={favorite} alt="favorite"/>
+                        <span className={styles.desc}>{this.props.likes}</span>
+                    </div>
+                    <div className={styles.bottom__item}>
+                        <img className={styles.bottom__img} src={chat} alt="chat"/>
+                        <span className={styles.desc}>{this.props.comments}</span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Card;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import styles from './home.module.scss';
+import Card from './Card';
+import axios from 'axios';
 
 class Home extends React.Component<any, any> {
+
+    state = {
+        card: []
+    }
+
+    async componentDidMount() {
+        try {
+            const response = await axios.get('card.json');
+            const data = response.data;
+            console.log(data)
+            this.setState({
+                card: data
+            });
+        } catch (e) {
+            console.log('error => ', e)
+        }
+    }
+
     render() {
 
         return (
@@ -17,6 +37,21 @@ class Home extends React.Component<any, any> {
                 </header>
                 <div>
                     <h4>This is a Home Page</h4>
+                    <div className={styles.wrapper}>
+                        {
+                            this.state.card.map((obj: any) => {
+                                return (
+                                    <Card
+                                        key={obj.id}
+                                        name = {obj.name}
+                                        description={obj.description}
+                                        likes={obj.likes}
+                                        comments={obj.comments}
+                                        image = {obj.image}
+                                    />)
+                            })
+                        }
+                    </div>
                 </div>
             </>
         );
